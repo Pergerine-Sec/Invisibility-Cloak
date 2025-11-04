369581ff97c021b4418c789b7cc160c1710f4587
Pergerine-Sec <pergerine.sec@tutamail.com>
2025-11-04 22:54:13 +0530

    Update main.py
    fixed the functionality to count ports, dosent count ports already counted; like will not count port 22 again .... [22, 22, 22, 22, ..., 22] 
    check if port number is already in list and adds accordingly

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import datetime 
+import datetime
 dat_cloak = {
-    
+
 }
+if len(dat_cloak)>60:
+    dat_cloak.clear()
+
 def ip_catcher(ip_addr):
     dat_cloak[ip_addr] = {
         "blocked" : 0,
@@ -11,21 +14,23 @@ def ip_catcher(ip_addr):
      }
 def connect_event(ip_addr, port):
         if ip_addr in dat_cloak:
-            dat_cloak[ip_addr]["ports"].append(port)
+            if port not in dat_cloak[ip_addr]["ports"]
+                dat_cloak[ip_addr]["ports"].append(port)
             dat_cloak[ip_addr]["attempts"]+=1
             dat_cloak[ip_addr]["last_attempt"]=datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
             if dat_cloak[ip_addr]["attempts"] >= 7 or len(dat_cloak[ip_addr]["ports"]) >= 5:
                 dat_cloak[ip_addr]["blocked"] = 1
             logger(ip_addr, port)
-        else: 
+        else:
             ip_catcher(ip_addr)
-            dat_cloak[ip_addr]["ports"].append(port)
+            if port not in dat_cloak[ip_addr]["ports"]
+                dat_cloak[ip_addr]["ports"].append(port)
             dat_cloak[ip_addr]["attempts"]+=1
             dat_cloak[ip_addr]["last_attempt"]=datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
             logger(ip_addr, port)
 def logger(ip_addr, port):
      time = dat_cloak[ip_addr]["last_attempt"]
-     #atn = access to network 
+     #atn = access to network
      if dat_cloak[ip_addr]["blocked"] == 0:
           atn = "ALLOWED"
      else:
@@ -40,4 +45,5 @@ null = ""
 connect_event(ip, 80)
 connect_event(ip, 443)
 connect_event(ip1, 8080)
+print(dat_cloak)
 print(dat_cloak)
